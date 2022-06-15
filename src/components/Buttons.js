c81e6b4faad7b1c8229e4f297f97be8da18c3446
mehmetozk4n6 <81770287+mehmetozk4n6@users.noreply.github.com>
2022-06-15 13:40:16 +0300

    eval deleted.Alternative solution used.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -32,25 +32,41 @@ function Buttons({
       setResult("");
       setIsCalculated(false);
     } else if (key === "=") {
-      if (transactation.length > 23) return;
-      setTransactation(transactation + key);
+      if (
+        transactation.length > 23 ||
+        transactation.match(/\d+[+\-*/]\d+/g) === null ||
+        transactation.match(/\d+[+\-*/]\d+/g).length < 1
+      )
+        return;
       setResult(Calculator(transactation));
+      setTransactation(transactation + key);
       setIsCalculated(true);
     } else {
       if (transactation.length > 23) return;
-      setTransactation(transactation + key);
       if (["+", "-", "*", "/"].includes(key)) {
+        if (transactation.length < 1) return;
+        // if (!transactation[transactation.length - 1] === "=") {
+        //   if (
+        //     transactation[transactation.length - 1]?.match(/\d+/) === null ||
+        //     transactation[transactation.length - 1]?.match(/\d+/).length < 1
+        //   )
+        //     return;
+        // }
         if (isCalculated) {
           setTransactation(result + key);
+          setResult(key);
           setIsCalculated(false);
+        } else {
+          setTransactation(transactation + key);
+          setResult(key);
         }
-        setResult(key);
       } else {
         if (isCalculated) {
           setTransactation(key);
           setResult(key);
           setIsCalculated(false);
         } else {
+          setTransactation(transactation + key);
           if (["+", "-", "*", "/"].includes(result[result.length - 1])) {
             setResult(key);
           } else {
@@ -85,11 +101,54 @@ function Buttons({
 
 export default Buttons;
 
-function Calculator(str) {
-  var arithmeticOperators = ["+", "-", "/", "*"];
-  var replacedStr = str.replace(/(.)\(/g, function (match, g1) {
-    if (arithmeticOperators.indexOf(g1) !== -1) return match;
-    else return match.replace(g1, g1 + "*");
-  });
-  return window.eval(replacedStr);
-}
+// function Calculator(str) {
+//   var arithmeticOperators = ["+", "-", "/", "*"];
+//   var replacedStr = str.replace(/(.)\(/g, function (match, g1) {
+//     if (arithmeticOperators.indexOf(g1) !== -1) return match;
+//     else return match.replace(g1, g1 + "*");
+//   });
+//   return eval(replacedStr);
+// }
+
+const Calculator = (operation) => {
+  const ops = [];
+
+  let num = 0;
+  for (let i = 0; i < operation.length; i++) {
+    if (["+", "-", "*", "/"].includes(operation[i])) {
+      ops.push(operation.slice(num, i));
+      ops.push(operation[i]);
+      num = i + 1;
+    } else if (i === operation.length - 1) {
+      ops.push(operation.slice(num, i + 1));
+    }
+  }
+
+  let opsSimple = [];
+
+  for (let x = 0; x < ops.length; x++) {
+    if (ops[x] === "*") {
+      const val = opsSimple.pop();
+      opsSimple.push(val * ops[x + 1]);
+      x++;
+    } else if (ops[x] === "/") {
+      // do not allow division by zero!
+      if (ops[x + 1] === "0") return "Error! Division by zero!";
+      const val = opsSimple.pop();
+      opsSimple.push(val / ops[x + 1]);
+      x++;
+    } else {
+      opsSimple.push(ops[x]);
+    }
+  }
+
+  let result = parseInt(opsSimple[0]);
+  for (let x = 1; x < opsSimple.length; x = x + 2) {
+    if (opsSimple[x] === "+") {
+      result += Number(opsSimple[x + 1]);
+    } else {
+      result -= opsSimple[x + 1];
+    }
+  }
+  return result;
+};
